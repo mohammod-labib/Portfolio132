70da1a45d424c5f72c02280c96d4c711a37484e6
Mohammod Labib <labibgbd25@gmail.com>
2025-11-29 17:02:37 +0600

    Add files via upload

---
+++ page2js/presentation/presentation.js
@@ -0,0 +1,33 @@
+                 //1st picture
+            function funn(){ 
+            document.getElementById("f1").style.visibility="visible";
+                document.getElementById("f1").classList.add("style");
+                document.getElementById("fe").style.visibility="visible";
+            document.getElementById("fe").classList.add("stylef");
+                          document.getElementById("fg").style.visibility="visible";
+            document.getElementById("fg").classList.add("styleg");
+            }
+            function removen(){
+             document.getElementById("fe").classList.remove("stylef");
+                document.getElementById("f1").classList.remove("style");
+                document.getElementById("fg").classList.remove("styleg");
+             document.getElementById("fe").style.visibility="hidden";
+            }
+            
+           //2nd picture 
+                    
+            function fun(){ 
+            document.getElementById("t1").style.visibility="visible";
+                document.getElementById("t1").classList.add("style2");
+                document.getElementById("te").style.visibility="visible";
+            document.getElementById("te").classList.add("stylef2");
+                          document.getElementById("tg").style.visibility="visible";
+            document.getElementById("tg").classList.add("styleg2");
+            }
+            function remove(){
+             document.getElementById("te").classList.remove("stylef2");
+                document.getElementById("t1").classList.remove("style2");
+                document.getElementById("tg").classList.remove("styleg2");
+             document.getElementById("te").style.visibility="hidden";
+            }
+            
